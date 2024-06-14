a65850da618dde9cd83f10d6908f1fb96adf8aba
Jonathan Haylett <jonathan@haylett.dev>
2024-06-14 22:45:01 +0100

    feat: add currently activated output to OLED dashboard

--- common/include/display/MenuButtonHints.hpp
+++ common/include/display/MenuButtonHints.hpp
@@ -1,11 +1,12 @@
 #ifndef _DISPLAY_MENUBUTTONHINTS_HPP
 #define _DISPLAY_MENUBUTTONHINTS_HPP
 
+#include "core/CommunicationBackend.hpp"
 #include "display/DisplayMode.hpp"
 
 class MenuButtonHints : public DisplayMode {
   public:
-    MenuButtonHints(CommunicationBackendId backend_id);
+    MenuButtonHints(CommunicationBackend **const backends, size_t backends_count);
     DisplayModeId GetId();
     void HandleControls(
         IntegratedDisplay *instance,
@@ -15,7 +16,14 @@ class MenuButtonHints : public DisplayMode {
     void UpdateDisplay(IntegratedDisplay *instance, Adafruit_GFX &display);
 
   protected:
-    CommunicationBackendId _backend_id;
+    const char *CurrentlyPressedButtonText();
+
+    CommunicationBackend **const _backends;
+    const size_t _backends_count;
+    CommunicationBackendId _backend_id = COMMS_BACKEND_UNSPECIFIED;
+
+    absolute_time_t _pressed_output_locked_until = 0;
+    const char *_last_pressed_button_text;
 };
 
 #endif
--- common/src/display/MenuButtonHints.cpp
+++ common/src/display/MenuButtonHints.cpp
@@ -4,8 +4,11 @@
 #include "core/config_utils.hpp"
 #include "util/state_util.hpp"
 
-MenuButtonHints::MenuButtonHints(const CommunicationBackendId backend_id)
-    : _backend_id(backend_id) {}
+MenuButtonHints::MenuButtonHints(CommunicationBackend **const backends, size_t backends_count)
+    : _backends(backends),
+      _backends_count(backends_count) {
+    _backend_id = backends[0]->BackendId();
+}
 
 DisplayModeId MenuButtonHints::GetId() {
     return DISPLAY_MODE_BUTTON_HINTS;
@@ -42,6 +45,23 @@ void MenuButtonHints::UpdateDisplay(IntegratedDisplay *instance, Adafruit_GFX &d
     display.setCursor(display.width() - (strlen(backend_text) * font_width), 0);
     display.print(backend_name(_backend_id));
 
+    /* Currently activated output */
+    display.setCursor(0, 20);
+    const char *currently_pressed_button_text = CurrentlyPressedButtonText();
+    // Lazy debounce on the output state, because it may be updated from the other core which could
+    // cause flickering. This debounce makes it so the button text has to change for 5 consecutive
+    // milliseconds before it will update on the display.
+    if (strcmp(currently_pressed_button_text, _last_pressed_button_text) == 0) {
+        _pressed_output_locked_until = 0;
+    } else {
+        if (_pressed_output_locked_until == 0) {
+            _pressed_output_locked_until = make_timeout_time_ms(5);
+        } else if (time_reached(_pressed_output_locked_until)) {
+            _last_pressed_button_text = currently_pressed_button_text;
+        }
+    }
+    display.print(_last_pressed_button_text);
+
     /* Button hints */
     uint8_t old_rotation = display.getRotation();
     display.setRotation(3);
@@ -57,4 +77,37 @@ void MenuButtonHints::UpdateDisplay(IntegratedDisplay *instance, Adafruit_GFX &d
     display.println("R3\n");
 
     display.setRotation(old_rotation);
+}
+
+const char *MenuButtonHints::CurrentlyPressedButtonText() {
+    OutputState &outputs = _backends[0]->GetOutputs();
+    uint32_t buttons = _backends[0]->GetOutputs().buttons;
+    for (uint8_t i = 0; i < _DigitalOutput_MAX; i++) {
+        bool activated = buttons & (1UL << i);
+        if (activated) {
+            return digital_output_name((DigitalOutput)(i + 1));
+        }
+    }
+    if (outputs.leftStickX < (128 - 5)) {
+        return "Left Stick X-";
+    } else if (outputs.leftStickX > (128 + 5)) {
+        return "Left Stick X+";
+    } else if (outputs.leftStickY < (128 - 5)) {
+        return "Left Stick Y-";
+    } else if (outputs.leftStickY > (128 + 5)) {
+        return "Left Stick Y+";
+    } else if (outputs.leftStickX < (128 - 5)) {
+        return "Right Stick X-";
+    } else if (outputs.leftStickX > (128 + 5)) {
+        return "Right Stick X+";
+    } else if (outputs.leftStickY < (128 - 5)) {
+        return "Right Stick Y-";
+    } else if (outputs.leftStickY > (128 + 5)) {
+        return "Right Stick Y+";
+    } else if (outputs.triggerLAnalog > 0) {
+        return "L2 Analog";
+    } else if (outputs.triggerRAnalog > 0) {
+        return "R2 Analog";
+    }
+    return "";
 }
